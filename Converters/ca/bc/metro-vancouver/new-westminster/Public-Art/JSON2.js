af737abdb85bcb03b36d7d7ebf266409058c5a15
D'Arcy Smith <darcy@terratap.com>
2018-02-09 12:01:45 -0800

    added new converter that doesn't rely on Node.js

---
+++ Converters/ca/bc/metro-vancouver/new-westminster/Public-Art/JSON2.js
@@ -0,0 +1,33 @@
+﻿module.exports =
+    {
+        doConversion: function(data)
+        {
+            return convert(data);
+        }
+    };
+
+function convert(data)
+{
+    var json      = JSON.parse(data);
+    var converted = {}
+
+    converted.type     = "FeatureCollection";
+    converted.features = []
+
+    var featuresJSON = json["features"];
+
+    for(var i = 0; i < featuresJSON.length; i++)
+    {
+        var feature = {}
+        var properties = {}
+
+        feature.type           = "Feature";
+        feature.geometry       = featuresJSON[i].geometry;
+        feature.properties     = properties
+        properties.name        = featuresJSON[i].properties.Name;
+        properties.description = featuresJSON[i].properties.Descriptn;
+        converted.features.push(feature)
+    }
+
+    return JSON.stringify(converted, null, 4);
+}
