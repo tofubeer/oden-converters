547efb9bdacb03496348e2fbeaba2c2765294405
D'Arcy Smith <darcy@terratap.com>
2018-02-24 07:46:20 -0800

    new conmverter for new west alternative fuels

---
+++ Converters/ca/bc/metro-vancouver/new-westminster/Alternative-Fuels-and-Electric-Charging-Stations/JSON2.js
@@ -0,0 +1,38 @@
+﻿module.exports =
+    {
+        doConvert: function(data)
+        {
+            return convert(data);
+        }
+    };
+
+function convert(data)
+{
+    const json         = JSON.parse(data);
+    var converted      = {};
+
+    converted.type     = "FeatureCollection";
+    converted.features = [];
+
+    var featuresJSON = json["features"];
+
+    for(var i = 0; i < featuresJSON.length; i++)
+    {
+        const featureJSON = featuresJSON[i];
+        var feature       = {};
+        var properties    = {};
+
+        feature.type       = "Feature";
+        feature.geometry   = featureJSON.geometry;
+        feature.properties = properties;
+        properties.name    = featureJSON.properties.Name
+        properties.type    = featureJSON.properties.Fuel
+        properties.address = featureJSON.properties.Location
+        properties.access  = featureJSON.properties.Access
+
+
+        converted.features.push(feature)
+    }
+
+    return JSON.stringify(converted, null, 4);
+}
