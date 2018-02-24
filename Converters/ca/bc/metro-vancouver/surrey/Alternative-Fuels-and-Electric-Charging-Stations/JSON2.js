ee418e43b5e4b60dcf4203acacb3cb60d201d5fd
D'Arcy Smith <darcy@terratap.com>
2018-02-24 08:08:52 -0800

    added converter for surrey alternative fuels JSON

---
+++ Converters/ca/bc/metro-vancouver/surrey/Alternative-Fuels-and-Electric-Charging-Stations/JSON2.js
@@ -0,0 +1,47 @@
+﻿module.exports =
+    {
+        doConvert: function(data)
+        {
+            return convert(data);
+        }
+    };
+
+function convert(data)
+{
+    const json        = JSON.parse(data);
+    const artistsJSON = json[0].features;
+    const worksJSON   = json[1].features;
+
+    const converted    = {};
+    converted.type     = "FeatureCollection";
+    converted.features = [];
+
+    for (var i = 0; i < json.length; i++)
+    {
+        const itemJSON = json[i];
+
+        var feature    = {};
+        var geometry   = {};
+        var properties = {};
+
+        feature.type         = "Feature";
+        feature.geometry     = geometry;
+        feature.properties   = properties;
+
+        geometry.type        = "Point";
+        geometry.coordinates =
+        [
+            itemJSON.LONGITUDE,
+            itemJSON.LATITUDE
+        ];
+
+        properties.name    = featureJSON.properties.NAME
+        properties.type    = featureJSON.properties.FUEL
+        properties.address = featureJSON.properties.LOCATION
+        properties.access  = featureJSON.properties.ACCESS
+
+        converted.features.push(feature)
+    }
+
+    return JSON.stringify(converted, null, 4);
+}
