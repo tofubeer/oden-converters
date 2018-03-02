d1aedd552488e2a5f7cba2e1cc33ca978d6b8b0e
D'Arcy Smith <darcy@terratap.com>
2018-03-02 08:59:43 -0800

    useful for debugging

---
+++ Converters/NullJSON.js
@@ -0,0 +1,12 @@
+﻿module.exports =
+    {
+        doConvert: function(data)
+        {
+            return convert(data);
+        }
+    };
+
+function convert(data)
+{
+    return JSON.stringify(data, null, 4);
+}
