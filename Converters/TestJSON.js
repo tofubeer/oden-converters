dfe9dde736cf3334f57469b2c5519737700d151b
D'Arcy Smith <darcy@terratap.com>
2018-03-02 09:03:53 -0800

    useful for debugging

---
+++ Converters/TestJSON.js
@@ -0,0 +1,18 @@
+﻿module.exports =
+    {
+        doConvert: function(data)
+        {
+            return convert(data);
+        }
+    };
+
+function convert(data)
+{
+    const json         = JSON.parse(data);
+    var converted      = {};
+
+    converted.type     = "FeatureCollection";
+    converted.features = [];
+    
+    return JSON.stringify(converted, null, 4);
+}
